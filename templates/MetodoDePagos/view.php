f7dc0c30faf1a472f515088f8fd6cf1ad6e09087
LUISOLMOS2025 <alemaniafc10@gmail.com>
2025-12-03 22:24:59 -0600

    Metodo de pago
    index y view y estilos

--- templates/MetodoDePagos/view.php
+++ templates/MetodoDePagos/view.php
@@ -4,81 +4,84 @@
  * @var \App\Model\Entity\MetodoDePago $metodoDePago
  */
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('Edit Metodo De Pago'), ['action' => 'edit', $metodoDePago->id], ['class' => 'side-nav-item']) ?>
-            <?= $this->Form->postLink(__('Delete Metodo De Pago'), ['action' => 'delete', $metodoDePago->id], ['confirm' => __('Are you sure you want to delete # {0}?', $metodoDePago->id), 'class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('List Metodo De Pagos'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('New Metodo De Pago'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
+<div class="metodoDePagos view content">
+    
+    <div style="display: flex; justify-content: space-between; align-items: center; margin-bottom: 24px;">
+        <h3><?= h($metodoDePago->alias) ?> <small style="color: #94a3b8; font-size: 0.6em; font-weight: 400;">(<?= h($metodoDePago->tipo_de_tarjeta) ?>)</small></h3>
+        <div class="actions-group">
+            <?= $this->Html->link(__('Volver'), ['action' => 'index'], ['class' => 'btn-ghost']) ?>
+            <?= $this->Html->link(__('Editar'), ['action' => 'edit', $metodoDePago->id], ['class' => 'btn-ghost', 'style' => 'color: var(--tx-accent); border-color: var(--tx-accent);']) ?>
+            <?= $this->Form->postLink(__('Borrar'), ['action' => 'delete', $metodoDePago->id], ['confirm' => __('¿Estás seguro de eliminar # {0}?', $metodoDePago->id), 'class' => 'btn-ghost', 'style' => 'color: #ef4444; border-color: #ef4444;']) ?>
+            <?= $this->Html->link(__('Nuevo'), ['action' => 'add'], ['class' => 'btn-accent']) ?>
         </div>
-    </aside>
-    <div class="column column-80">
-        <div class="metodoDePagos view content">
-            <h3><?= h($metodoDePago->tipo_de_tarjeta) ?></h3>
-            <table>
-                <tr>
-                    <th><?= __('Tipo De Tarjeta') ?></th>
-                    <td><?= h($metodoDePago->tipo_de_tarjeta) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Nombre Del Titular') ?></th>
-                    <td><?= h($metodoDePago->nombre_del_titular) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Cvv') ?></th>
-                    <td><?= h($metodoDePago->cvv) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Fecha De Vencimiento') ?></th>
-                    <td><?= h($metodoDePago->fecha_de_vencimiento) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Alias') ?></th>
-                    <td><?= h($metodoDePago->alias) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('User') ?></th>
-                    <td><?= $metodoDePago->hasValue('user') ? $this->Html->link($metodoDePago->user->username, ['controller' => 'Users', 'action' => 'view', $metodoDePago->user->id]) : '' ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Id') ?></th>
-                    <td><?= $this->Number->format($metodoDePago->id) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Created') ?></th>
-                    <td><?= h($metodoDePago->created) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Modified') ?></th>
-                    <td><?= h($metodoDePago->modified) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Es Predeterminado') ?></th>
-                    <td><?= $metodoDePago->es_predeterminado ? __('Yes') : __('No'); ?></td>
-                </tr>
-            </table>
-            <div class="related">
-                <h4><?= __('Related Viajes') ?></h4>
-                <?php if (!empty($metodoDePago->viajes)) : ?>
-                <div class="table-responsive">
-                    <table>
+    </div>
+
+    <div class="view-card">
+        <table>
+            <tr>
+                <th><?= __('Alias') ?></th>
+                <td><?= h($metodoDePago->alias) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Tipo De Tarjeta') ?></th>
+                <td><?= h($metodoDePago->tipo_de_tarjeta) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Nombre Del Titular') ?></th>
+                <td><?= h($metodoDePago->nombre_del_titular) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('CVV') ?></th>
+                <td>***</td> </tr>
+            <tr>
+                <th><?= __('Fecha De Vencimiento') ?></th>
+                <td><?= h($metodoDePago->fecha_de_vencimiento) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Usuario Propietario') ?></th>
+                <td><?= $metodoDePago->hasValue('user') ? $this->Html->link($metodoDePago->user->username, ['controller' => 'Users', 'action' => 'view', $metodoDePago->user->id]) : '' ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Es Predeterminado') ?></th>
+                <td>
+                    <?php if($metodoDePago->es_predeterminado): ?>
+                        <span style="color: #10b981; font-weight: bold;">Sí</span>
+                    <?php else: ?>
+                        No
+                    <?php endif; ?>
+                </td>
+            </tr>
+            <tr>
+                <th><?= __('ID del Registro') ?></th>
+                <td><?= $this->Number->format($metodoDePago->id) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Creado') ?></th>
+                <td><?= h($metodoDePago->created) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Modificado') ?></th>
+                <td><?= h($metodoDePago->modified) ?></td>
+            </tr>
+        </table>
+    </div>
+
+    <div class="related">
+        <?php if (!empty($metodoDePago->viajes)) : ?>
+            <h4><?= __('Viajes pagados con este método') ?></h4>
+            <div class="table-responsive">
+                <table>
+                    <thead>
                         <tr>
                             <th><?= __('Id') ?></th>
                             <th><?= __('Hora Inicio') ?></th>
                             <th><?= __('Hora Fin') ?></th>
                             <th><?= __('Costo Total') ?></th>
-                            <th><?= __('Estado Viaje') ?></th>
-                            <th><?= __('User Id') ?></th>
-                            <th><?= __('Vehiculo Id') ?></th>
-                            <th><?= __('Metodo De Pago Id') ?></th>
-                            <th><?= __('Estacion Id') ?></th>
-                            <th><?= __('Promocion Id') ?></th>
-                            <th><?= __('Created') ?></th>
-                            <th><?= __('Modified') ?></th>
-                            <th class="actions"><?= __('Actions') ?></th>
+                            <th><?= __('Estado') ?></th>
+                            <th class="actions"><?= __('Acciones') ?></th>
                         </tr>
+                    </thead>
+                    <tbody>
                         <?php foreach ($metodoDePago->viajes as $viaje) : ?>
                         <tr>
                             <td><?= h($viaje->id) ?></td>
@@ -86,31 +89,27 @@
                             <td><?= h($viaje->hora_fin) ?></td>
                             <td><?= h($viaje->costo_total) ?></td>
                             <td><?= h($viaje->estado_viaje) ?></td>
-                            <td><?= h($viaje->user_id) ?></td>
-                            <td><?= h($viaje->vehiculo_id) ?></td>
-                            <td><?= h($viaje->metodo_de_pago_id) ?></td>
-                            <td><?= h($viaje->estacion_id) ?></td>
-                            <td><?= h($viaje->promocion_id) ?></td>
-                            <td><?= h($viaje->created) ?></td>
-                            <td><?= h($viaje->modified) ?></td>
                             <td class="actions">
-                                <?= $this->Html->link(__('View'), ['controller' => 'Viajes', 'action' => 'view', $viaje->id]) ?>
-                                <?= $this->Html->link(__('Edit'), ['controller' => 'Viajes', 'action' => 'edit', $viaje->id]) ?>
+                                <?= $this->Html->link(__('Ver'), ['controller' => 'Viajes', 'action' => 'view', $viaje->id]) ?>
+                                <?= $this->Html->link(__('Editar'), ['controller' => 'Viajes', 'action' => 'edit', $viaje->id]) ?>
                                 <?= $this->Form->postLink(
-                                    __('Delete'),
+                                    __('Borrar'),
                                     ['controller' => 'Viajes', 'action' => 'delete', $viaje->id],
                                     [
                                         'method' => 'delete',
-                                        'confirm' => __('Are you sure you want to delete # {0}?', $viaje->id),
+                                        'confirm' => __('¿Borrar viaje # {0}?', $viaje->id),
                                     ]
                                 ) ?>
                             </td>
                         </tr>
                         <?php endforeach; ?>
-                    </table>
-                </div>
-                <?php endif; ?>
+                    </tbody>
+                </table>
             </div>
-        </div>
+        <?php else: ?>
+            <div class="empty-state">
+                <p>No hay viajes registrados con este método de pago.</p>
+            </div>
+        <?php endif; ?>
     </div>
 </div>
